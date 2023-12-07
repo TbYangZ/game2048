7124905bcbfa512776f55a7e3a063cef339158d1
TbYangZ <1104524667@qq.com>
2023-12-08 00:54:08 +0800

    12/8: Images of game

--- src/main/java/com/example/game2048/ControllerMainGame.java
+++ src/main/java/com/example/game2048/ControllerMainGame.java
@@ -2,10 +2,41 @@ package com.example.game2048;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class ControllerMainGame {
-    @FXML
-    private BorderPane borderPane;
+
+    public ImageView main_bg;
+    public BorderPane borderPane;
+    public ImageView cell00, cell01, cell02, cell03;
+    public ImageView cell10, cell11, cell12, cell13;
+    public ImageView cell20, cell21, cell22, cell23;
+    public ImageView cell30, cell31, cell32, cell33;
+    public ImageView[][] cells = new ImageView[4][4];
+    private final int basicX = 200;
+    private final int basicY = 25;
+    private final int cellWidth = 95;
+    public void initialize() {
+        main_bg.setLayoutX(basicX);
+        main_bg.setLayoutY(basicY);
+        cells[0][0] = cell00; cells[0][1] = cell01; cells[0][2] = cell02; cells[0][3] = cell03;
+        cells[1][0] = cell10; cells[1][1] = cell11; cells[1][2] = cell12; cells[1][3] = cell13;
+        cells[2][0] = cell20; cells[2][1] = cell21; cells[2][2] = cell22; cells[2][3] = cell23;
+        cells[3][0] = cell30; cells[3][1] = cell31; cells[3][2] = cell32; cells[3][3] = cell33;
+        for (int i = 0 ; i < 4; ++i)
+            for (int j = 0; j < 4; ++j) {
+                int x = basicX + (j + 1) * 4 + j * cellWidth;
+                int y = basicY + (i + 1) * 4 + i * cellWidth;
+                cells[i][j].setLayoutX(x);
+                cells[i][j].setLayoutY(y);
+            }
+    }
+    public void setCell(int x, int y, int number) {
+        String imgPath = getClass().getResource("number_" + number + ".png").toExternalForm();
+        cells[x][y].setImage(new Image(imgPath));
+    }
 }
--- src/main/java/com/example/game2048/Main.java
+++ src/main/java/com/example/game2048/Main.java
@@ -12,11 +12,12 @@ public class Main extends Application {
     private final double Height = 900.0;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), Width / 2, Height / 2);
         stage.setTitle("Game 2048");
         stage.setScene(scene);
-        scene.getStylesheets().add(Main.class.getResource("main-bg.css").toExternalForm());
+        stage.setResizable(false);
+//        scene.getStylesheets().add(Main.class.getResource("main-bg.css").toExternalForm());
         stage.show();
     }
 
@@ -24,3 +25,39 @@ public class Main extends Application {
         launch();
     }
 }
+
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.GridPane;
+//import javafx.stage.Stage;
+//
+//public class Main extends Application {
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+//        GridPane gridPane = new GridPane();
+//
+//        // 创建几个按钮，并设置它们在 GridPane 中的位置
+//        Button button1 = new Button("Button 1");
+//        gridPane.add(button1, 0, 0);  // 第一列，第一行
+//
+//        Button button2 = new Button("Button 2");
+//        gridPane.add(button2, 1, 0);  // 第二列，第一行
+//
+//        Button button3 = new Button("Button 3");
+//        gridPane.add(button3, 0, 1);  // 第一列，第二行
+//
+//        Button button4 = new Button("Button 4");
+//        gridPane.add(button4, 1, 1);  // 第二列，第二行
+//
+//        Scene scene = new Scene(gridPane, 300, 200);
+//        primaryStage.setScene(scene);
+//        primaryStage.setTitle("GridPane Example");
+//        primaryStage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
